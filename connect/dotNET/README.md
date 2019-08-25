2bf26113380c768e3f612ec02008945e1b7092a8
David Proksch <david.proksch@gmail.com>
2019-08-25 15:40:17 -0400

    Adding Stuff

--- connect/dotNET/README.md
+++ connect/dotNET/README.md
@@ -4,10 +4,40 @@
 ```
 dotnet add package Snowflake.Data
 ```
-1.  Configure the environment
+2.  Configure the environment
     1.  Copy the env environment file to a .env file
     1.  Edit the .env file, configure for your environment
     1.  Source the .env file
     ```
     . .env
     ```
+3.  Run the Program.cs file
+$ dotnet run
+Begin Snowflake|dotnet Connection Demo!
+0 - ALGERIA
+1 - ARGENTINA
+2 - BRAZIL
+3 - CANADA
+4 - EGYPT
+5 - ETHIOPIA
+6 - FRANCE
+7 - GERMANY
+8 - INDIA
+9 - INDONESIA
+10 - IRAN
+11 - IRAQ
+12 - JAPAN
+13 - JORDAN
+14 - KENYA
+15 - MOROCCO
+16 - MOZAMBIQUE
+17 - PERU
+18 - CHINA
+19 - ROMANIA
+20 - SAUDI ARABIA
+21 - VIETNAM
+22 - RUSSIA
+23 - UNITED KINGDOM
+24 - UNITED STATES
+End Snowflake|dotnet Connection Demo!
+```
---
+++ connect/dotNET/SnowflakeConnectionDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using Snowflake.Data.Client;
+using Snowflake.Data.Core;
+
+
+namespace SnowflakeConnectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Begin Snowflake|dotnet Connection Demo!");
+			
+            string account = Environment.GetEnvironmentVariable("SF_ACCOUNT");
+            string user = Environment.GetEnvironmentVariable("SF_USER");
+            string pswd = Environment.GetEnvironmentVariable("SF_PASSWORD");
+            string db = Environment.GetEnvironmentVariable("SF_DB");
+            string schema = Environment.GetEnvironmentVariable("SF_SCHEMA");
+            string wh = Environment.GetEnvironmentVariable("SF_WAREHOUSE");
+            String connString = $"ACCOUNT={account};USER={user};PASSWORD={pswd};DB={db};SCHEMA={schema};WAREHOUSE={wh}";
+
+//DB={db};WAREHOUSE={wh}";
+
+			using (IDbConnection conn = new SnowflakeDbConnection())
+            {
+				conn.ConnectionString = connString;
+				conn.Open();
+
+				IDbCommand cmd = conn.CreateCommand();
+				cmd.CommandText = "select n_nationkey, n_name from nation";
+				String nationKey;
+				String nationName;
+                IDataReader reader = cmd.ExecuteReader();
+				while(reader.Read())
+				{
+				
+					nationKey = reader.GetString(0);
+					nationName = reader.GetString(1);
+					Console.WriteLine($"{nationKey} - {nationName}");
+				}
+				conn.Close();
+            }	
+            Console.WriteLine("End Snowflake|dotnet Connection Demo!");
+        }
+    }
+}
